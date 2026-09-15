538d68662969ea81ec1b929d0bc9e8b888d4fb26
Izzuddin <izzuddinfathi0@gmail.com>
2026-09-15 21:21:51 +0800

    fix: Add incomes relationship to User model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -35,4 +35,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Expense::class);
     }
+
+    public function incomes()
+    {
+        return $this->hasMany(Income::class);
+    }
 }
